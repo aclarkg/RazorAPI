50358f21dafa4c07560a5da2ce6452b0c1fd04cf
Alan Gulick <ACG216@louisvillemsd.org>
2018-11-06 14:37:55 -0500

    First commit

--- RazorAPI/Models/Car.cs
+++ RazorAPI/Models/Car.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorAPI.Pages
+namespace RazorAPI.Models
 {
-    public class IndexModel : PageModel
+    public class Car
     {
         public int Id { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
-        public string Colour { get; set; }
         public int Doors { get; set; }
-        public int Price { get; set; }
+        public string Colour { get; set; }
+        public decimal Price { get; set; }
+
     }
 }
--- RazorAPI/Startup.cs
+++ RazorAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RazorAPI.Services;
 
 namespace RazorAPI
 {
@@ -24,15 +25,9 @@ namespace RazorAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddTransient<ICarService, CarService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
